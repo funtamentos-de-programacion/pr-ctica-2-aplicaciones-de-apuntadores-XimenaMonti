2b959f86058252c47b7677a815f8e37a501f722d
XimenaMontielGuerrero <xmontielguerrero@gmail.com>
2025-02-27 22:02:17 -0600

    Archivos

--- ejercicio4.c
+++ ejercicio4.c
@@ -6,16 +6,19 @@ int main() {
   
   printf("Valores originales:\n");
   for (int i = 0; i < 5; i++) {
+    printf("%d ", *(ptr + i));  
     //Escribe el codigo para imprimir los valores usado el apuntador
   }
   
   for (int i = 0; i < 5; i++) {
-    // Duplicar cada elemento
+    *(ptr + i) *= 2;// Duplicar cada elemento
+
   }
   
   printf("\nValores modificados:\n");
   for (int i = 0; i < 5; i++) {
     //Escribe el codigo para imprimir los valores usado el apuntador
+    printf("%d ", *(ptr + i));
   }
   return 0;
 }
